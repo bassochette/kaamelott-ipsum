6976c505e993ffdf927b700f76104145c976b56b
julien <julien.prugne@gmail.com>
2017-10-14 16:13:41 -0400

    manage NaN cases

--- server.js
+++ server.js
@@ -5,16 +5,17 @@ const app = express();
 app.get(
     "/:quotes_to_display",
     (request, response) => {
-        const quotes_to_display = request.params.quotes_to_display;
-        const oneQuote = () => quotes[Math.floor(Math.random()*quotes.length)];
+        let quotes_to_display = parseInt(request.params.quotes_to_display)
+        if (isNaN(quotes_to_display)) quotes_to_display = 10
+        const oneQuote = () => quotes[Math.floor(Math.random()*quotes.length)]
         const xQuotes = (x) => {
             let quotes = "";
             for (let i =0; i < x; i++) {
-                quotes += "\n"+oneQuote();
+                quotes += "\n"+oneQuote()
             }
-            return quotes;
+            return quotes
         }
-        response.status(200).send(xQuotes(parseInt(quotes_to_display)));
+        response.status(200).send(xQuotes(parseInt(quotes_to_display)))
     }
 );
 
